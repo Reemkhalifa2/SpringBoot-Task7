d8ef36b4952acc64542d3cc40066375733e7c6fe
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-07 15:06:11 +0400

    task 36

---
+++ src/main/java/com/example/Task7Demo/Product.java
@@ -0,0 +1,15 @@
+package com.example.Task7Demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Product {
+    private Integer productId;
+    private String productName;
+    private Double price;
+
+}
