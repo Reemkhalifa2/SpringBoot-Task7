d42327884deade495c07370133db054fadebeee5
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-07 14:34:40 +0400

    first commit

---
+++ src/main/java/com/example/Task7Demo/Task7DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Task7Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task7DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task7DemoApplication.class, args);
+	}
+
+}
