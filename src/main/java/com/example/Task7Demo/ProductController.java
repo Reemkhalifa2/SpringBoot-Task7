672a8c8c696ed9595bd55fcc54289175f8e4808c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-08 07:55:30 +0400

    Task 36: Building a Java Spring Boot DELETE Endpoint – Remove a Product from Inventory Example

---
+++ src/main/java/com/example/Task7Demo/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.Task7Demo;
+
+import com.example.Task7Demo.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductService productService = new ProductService();
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable int id){
+        productService.deleteProductById(id);
+
+    }
+
+
+}
